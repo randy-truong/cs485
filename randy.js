6bd79419abe2c08eb20b10161722f983377337dc
Randy Truong <rndtrng@gmail.com>
2022-02-27 19:58:11 -0500

    update cross highlighting

--- randy.js
+++ randy.js
@@ -1,55 +1,77 @@
 var vlSpec = {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "data": {
-    "url": "https://raw.githubusercontent.com/randy-truong/cs485/main/assets/data/origin_state_sum.csv"
-    },
-  "transform": [
-    {"filter": {"field": "DATE", "equal": "2019-01"}}
-  ],
-  "params": [{"name": "highlight", "select": "point"}],
-  "mark": {"type": "rect", "strokeWidth": 2},
-  "encoding": {
-    "y": {
-      "field": "ORIGIN_STATE_NM",
-      "type": "nominal"
-    },
-    "x": {
-      "field": "DEST_STATE_NM",
-      "type": "nominal"
-    },
-    "fill": {
-      "field": "PASSENGERS",
-      "type": "quantitative"
-    },
-    "stroke": {
-      "condition": {
-        "param": "highlight",
-        "empty": false,
-        "value": "black"
+  "description": "A dashboard with cross-highlighting.",
+  "data": {"url": "https://raw.githubusercontent.com/randy-truong/cs485/main/assets/data/origin_state_sum.csv"},
+  "vconcat": [
+    {
+      "layer": [{
+        "transform": [
+          {"filter": "datum.DATE === toString('2019-01')"}
+        ],
+        "title": "Monthly Traveler Mobility Between States",
+        "mark": "rect",
+        "encoding": {
+          "x": {
+            "type": "nominal",
+            "field": "ORIGIN_STATE_NM",
+            "title": "Origin State"
+          },
+          "y": {
+            "type": "nominal",
+            "field": "DEST_STATE_NM",
+            "title": "Destination State"
+          },
+          "fill": {
+            "field": "PASSENGERS",
+            "type": "quantitative",
+            "title": "# Travelers"
+          },
+          "color": {
+            "legend": {
+              "title": "# Passengers Count",
+              "direction": "horizontal",
+              "gradientLength": 120
+            }
+          },
+          "opacity": {
+            "condition": {"param": "pts", "value": 1},
+            "value": 0.2
+          }
+        }
+      }]
+    }, {
+
+      "transform": [
+          {"filter": "datum.DATE === toString('2019-01')"}
+      ],
+      "title": "Monthly Total Number of Travelers Departing by State",
+      "mark": "bar",
+      "params": [{
+        "name": "pts",
+        "select": {"type": "point", "encodings": ["x"]}
+      }],
+      "encoding": {
+        "x": {"field": "ORIGIN_STATE_NM", "type": "nominal", "title": "Origin State"},
+        "y": {"field": "PASSENGERS", "type": "quantitative", "title": "Number of Travelers"},
+        "color": {
+          "condition": {
+            "param": "pts",
+            "value": "steelblue"
+          },
+          "value": "grey"
+        }
       },
-      "value": null
-    },
-    "opacity": {
-      "condition": {"param": "highlight", "value": 1},
-      "value": 0.5
-    },
-    "order": {"condition": {"param": "highlight", "value": 1}, "value": 0}
-  },
-  "config": {
-    "scale": {
-      "bandPaddingInner": 0,
-      "bandPaddingOuter": 0
-    },
-    "view": {"step": 15},
-    "range": {
-      "ramp": {
-        "scheme": "yellowgreenblue"
-      }
-    },
-    "axis": {
-      "domain": false
+      
     }
-  }
+  ],
+  "resolve": {
+    "legend": {
+      "color": "independent",
+      "size": "independent"
+    }
+  },
+  "config": {"view": {"step": 12}}
 };
+
 
 vegaEmbed('#randy', vlSpec);
